6ccc97e20ce08bb3615cb35618407087a0670b64
imal chanaka <107134405+imalchanaka@users.noreply.github.com>
2024-04-07 14:15:12 +0530

    get all users and get role

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -13,12 +13,13 @@ const loginUser = async (req, res) => {
   const {email, password} = req.body
 
   try {
-    const user = await User.login(email, password)
+    const {_id,role} = await User.login(email, password)
 
     // create a token
-    const token = createToken(user._id)
+    const token = createToken(_id)
+    
 
-    res.status(200).json({email, token})
+    res.status(200).json({email, token,role})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
